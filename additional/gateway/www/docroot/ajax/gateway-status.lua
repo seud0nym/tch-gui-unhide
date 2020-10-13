eafbe73c2f5b630b9ad9993caf321c1b3487f37b
seud0nym <seud0nym@yahoo.com.au>
2020-10-13 17:13:40 +1100

    Added sensor temperatures to gateway card

--- additional/gateway/www/docroot/ajax/gateway-status.lua
+++ additional/gateway/www/docroot/ajax/gateway-status.lua
@@ -1,11 +1,17 @@
 local json = require("dkjson")
 local proxy = require("datamodel")
 local readfile = require("web.content_helper").readfile
+local floor, ipairs = math.floor, ipairs
 local ngx = ngx
 
 local ram_total = tonumber(proxy.get("sys.mem.RAMTotal")[1].value or 0) or 0
 local ram_free = tonumber(proxy.get("sys.mem.RAMFree")[1].value or 0) or 0
 local cpu_usage = proxy.get("sys.proc.CPUUsage")[1].value or "0"
+local temp1_input = {}
+local temp_output = {}
+for i,f in ipairs(temp1_input) do
+  temp_output[i] = readfile(f,"number",floor) / 1000
+end
 
 local data = {
   cpu = cpu_usage,
@@ -14,6 +20,7 @@ local data = {
   uptime = readfile("/proc/uptime","number",floor),
   time = os.date("%d/%m/%Y %H:%M:%S",os.time()),
   load = readfile("/proc/loadavg","string"):sub(1,14),
+  temps = table.concat(temp_output, "&deg; ") .. "&deg;",
 }
 
 local buffer = {}
