e1b3362886ed748288a8ed0518a98f0ada3bd285
seud0nym <seud0nym@yahoo.com.au>
2021-01-06 10:19:40 +1100

    Fixed device counts on FW 17.2

--- additional/devices/www/lua/devicescard_helper.lua
+++ additional/devices/www/lua/devicescard_helper.lua
@@ -13,12 +13,12 @@ function M.getDevicesCardHTML()
   local devices_data = {
     numWireless = "sys.hosts.ActiveWirelessNumberOfEntries",
     numEthernet = "sys.hosts.ActiveEthernetNumberOfEntries",
-    controller_enabled = "uci.multiap.controller.enabled",
   }
   content_helper.getExactContent(devices_data)
 
-  local multiap = (devices_data["controller_enabled"] and devices_data["controller_enabled"] == "1")
   local nAgtDevices = 0
+  local multiap_controller_enabled = proxy.get("uci.multiap.controller.enabled")
+  local multiap = (multiap_controller_enabled and multiap_controller_enabled[1].value == "1")
   if multiap then
     local agents = proxy.get("Device.Services.X_TELSTRA_MultiAP.Controller.MultiAPAgentNumberOfEntries")
     if agents and tonumber(agents[1].value) > 0 then
@@ -33,23 +33,23 @@ function M.getDevicesCardHTML()
 
   local nEth = tonumber(devices_data["numEthernet"]) or 0
   local nWiFi = tonumber(devices_data["numWireless"]) or 0
-  if nAgtDevices > 0 and nEth > 0 then
+  if multiap and nAgtDevices > 0 and nEth > 0 then
     nEth = nEth - nAgtDevices
   end
 
   local html = {}
 
   html[#html+1] = '<span class="simple-desc">'
-  html[#html+1] = '<i class="icon-desktop status-icon" style="font-size:12px">&nbsp;</i>'
+  html[#html+1] = '<i class="icon-link status-icon"></i>'
   html[#html+1] = format(N('<strong %1$s>%2$d ethernet device</strong> connected','<strong %1$s>%2$d ethernet devices</strong> connected',nEth),modal_link,nEth)
   html[#html+1] = '</span>'
   html[#html+1] = '<span class="simple-desc">'
-  html[#html+1] = '<i class="icon-tablet status-icon" style="text-align:center">&nbsp;</i>'
+  html[#html+1] = '<i class="icon-wifi status-icon"></i>'
   html[#html+1] = format(N('<strong %1$s>%2$d Wi-Fi device</strong> connected','<strong %1$s>%2$d Wi-Fi devices</strong> connected',nWiFi),modal_link,nWiFi)
   html[#html+1] = '</span>'
   if multiap then
     html[#html+1] = '<span class="simple-desc">'
-    html[#html+1] = '<i class="icon-sitemap status-icon">&nbsp;</i>'
+    html[#html+1] = '<i class="icon-sitemap status-icon"></i>'
     html[#html+1] = format(N('<strong %1$s>%2$d Wi-Fi device</strong> booster connected','<strong %1$s>%2$d Wi-Fi devices</strong> booster connected',nAgtDevices),modal_link,nAgtDevices)
     html[#html+1] = '</span>'
   end
