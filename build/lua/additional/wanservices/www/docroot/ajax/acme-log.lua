5361f529fa73b04fcda36e41943c8eb0daabe946
seud0nym <seud0nym@yahoo.com.au>
2021-06-01 15:02:43 +1000

    Auto-refresh certificate log

---
+++ build/lua/additional/wanservices/www/docroot/ajax/acme-log.lua
@@ -0,0 +1,21 @@
+local json = require("dkjson")
+local proxy = require ("datamodel")
+
+local log, err = proxy.get("rpc.gui.acme.log")
+if log then
+  log = log[1].value
+else
+  log = err
+end
+
+local data = {
+  html = log,
+}
+
+local buffer = {}
+if json.encode (data, { indent = false, buffer = buffer }) then
+  ngx.say(buffer)
+else
+  ngx.say("{}")
+end
+ngx.exit(ngx.HTTP_OK)
