702cee3b0df60d322bb08b85033197b182755f07
seud0nym <seud0nym@yahoo.com.au>
2021-06-07 17:46:32 +1000

    Added file system free space to Gateway card

--- build/lua/additional/gateway/www/docroot/ajax/gateway-status.lua
+++ build/lua/additional/gateway/www/docroot/ajax/gateway-status.lua
@@ -4,6 +4,11 @@ local readfile = require("web.content_helper").readfile
 local floor, ipairs = math.floor, ipairs
 local ngx = ngx
 
+local cmd = io.popen("df -h  / | grep ^overlay")
+local overlay = cmd:read()
+cmd:close()
+
+local disk_total, disk_free = string.match(overlay, "%S+%s+(%S+)%s+%S+%s+(%S+).*")
 local ram_total = tonumber(proxy.get("sys.mem.RAMTotal")[1].value or 0) or 0
 local ram_free = tonumber(proxy.get("sys.mem.RAMFree")[1].value or 0) or 0
 local cpu_usage = proxy.get("sys.proc.CPUUsage")[1].value or "0"
@@ -17,6 +22,8 @@ local data = {
   cpu = cpu_usage,
   ram_free = ram_free,
   ram_total = ram_total,
+  disk_free = disk_free,
+  disk_total = disk_total,
   uptime = readfile("/proc/uptime","number",floor),
   time = os.date("%d/%m/%Y %H:%M:%S",os.time()),
   load = readfile("/proc/loadavg","string"):sub(1,14),
