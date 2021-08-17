091dcab970e02daccf414e07e5c102e299a8b502
seud0nym <seud0nym@yahoo.com.au>
2021-08-17 16:46:55 +1000

    Allow remove and re-apply when testing

--- extras/xtra-test.sh
+++ extras/xtra-test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-while getopts :rx option
+while getopts :arx option
 do
  case "${option}" in
+  a)  REAPPLY=Y;;
   r)  XTRAS_REMOVE=Y;;
   x)  set -x;;
-  *)  echo "Usage: $0 [-x] [-r] tch-gui-unhide-xtra-<script>"; exit;;
+  *)  echo "Usage: $0 [-x] [-r [-a]] tch-gui-unhide-xtra-<script>"; exit;;
  esac
 done
 shift $((OPTIND-1))
@@ -20,9 +21,14 @@ FW_BASE=$(uci get version.@version[0].marketing_version)
 SRV_transformer=0
 
 . ./$1
+if [ "$XTRAS_REMOVE" = "Y" -a "$REAPPLY" = "Y" ]; then
+  XTRAS_REMOVE="" 
+  . ./$1
+fi
 
 if [ $SRV_transformer -gt 0 ]; then
   /etc/init.d/watchdog-tch stop
   /etc/init.d/transformer restart
   /etc/init.d/watchdog-tch start
 fi
+/etc/init.d/nginx restart
