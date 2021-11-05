8ab19669b1fcbaa870a6eb59af49e9674c8264d8
seud0nym <seud0nym@yahoo.com.au>
2021-11-05 21:10:47 +1100

    Cosmetic changes

--- build/lua/additional/common/wanservices/www/lua/wanservicescard_helper.lua
+++ build/lua/additional/common/wanservices/www/lua/wanservicescard_helper.lua
@@ -1,28 +1,31 @@
 local proxy = require("datamodel")
 local ui_helper = require("web.ui_helper")
 local content_helper = require("web.content_helper")
-local gmatch, format, lower, match = string.gmatch, string.format, string.lower, string.match
+local gmatch,format,lower,match,untaint = string.gmatch,string.format,string.lower,string.match,string.untaint
 
 local M = {}
 
-local function get_ddns_status(wan_services_data, family)
-  local service_name = format("myddns_%s", lower(family))
-  local state_prefix = format("%s Dynamic DNS ", family)
-  local ddnsstate
+local function get_ddns_status(wan_services_data,family)
+  local ddns_state
 
   if wan_services_data["ddns_"..family.."_enabled"] ~= "1" then
-    ddnsstate = ui_helper.createSimpleLight("0", T(state_prefix .. "disabled"))
+    ddns_state = ui_helper.createSimpleLight("0",T(format("%s Dynamic DNS disabled",family)))
   else
+    local service_name = format("myddns_%s",lower(family))
+    local name = untaint(wan_services_data["ddns_"..family.."_domain"])
+    local attr = { span = { title = name }}
     local cert = ""
     if wan_services_data["cert_"..family.."_enabled"] and wan_services_data["cert_"..family.."_enabled"] == "1" then
-      cert="&nbsp;<span class='icon-lock' title='Server Certificate auto-renewal enabled'></span>"
+      cert="&nbsp;<span class='icon-lock' title='Server Certificate auto-renewal enabled for "..name.."'></span>"
+    else
+      cert="&nbsp;<span class='icon-unlock' title='Server Certificate auto-renewal disabled for "..name.."'></span>"
     end
 
     local status = wan_services_data["ddns_status"]
     local service_status
     if status then
-      for x in gmatch(status, '([^%]]+)') do
-        service_status = match(x, service_name .. "%[(.+)")
+      for x in gmatch(status,'([^%]]+)') do
+        service_status = match(x,service_name.."%[(.+)")
         if service_status then
           break
         end
@@ -30,19 +33,20 @@ local function get_ddns_status(wan_services_data, family)
     end
     
     if service_status then
+      local state_prefix = format("%s Dynamic DNS ",family)
       if service_status == "Domain's IP updated" then
-        ddnsstate = ui_helper.createSimpleLight("1", T(state_prefix .. "- IP updated" .. cert))
+        ddns_state = ui_helper.createSimpleLight("1",T(state_prefix.."- IP updated"..cert),attr)
       elseif service_status == "No error received from server" then
-        ddnsstate = ui_helper.createSimpleLight("2", T(state_prefix .. "- IP updating" .. cert))
+        ddns_state = ui_helper.createSimpleLight("2",T(state_prefix.."- IP updating"..cert),attr)
       else
-        ddnsstate = ui_helper.createSimpleLight("4", T(state_prefix .. "update error" .. cert))
+        ddns_state = ui_helper.createSimpleLight("4",T(state_prefix.."update error"..cert),attr)
       end
     else
-      ddnsstate = ui_helper.createSimpleLight("4", T(state_prefix .. "state unknown" .. cert))
+      ddns_state = ui_helper.createSimpleLight("4",T(state_prefix.."state unknown"..cert),attr)
     end
   end
 
-  return ddnsstate
+  return ddns_state
 end
 
 function M.getWANServicesCardHTML()
@@ -54,29 +58,31 @@ function M.getWANServicesCardHTML()
     upnp_status = "uci.upnpd.config.enable_upnp",
     upnp_rules = "sys.upnp.RedirectNumberOfEntries",
     ddns_IPv4_enabled = "uci.ddns.service.@myddns_ipv4.enabled",
+    ddns_IPv4_domain = "uci.ddns.service.@myddns_ipv4.domain",
     ddns_IPv6_enabled = "uci.ddns.service.@myddns_ipv6.enabled",
+    ddns_IPv6_domain = "uci.ddns.service.@myddns_ipv6.domain",
     ddns_status = "rpc.ddns.status",
     cert_IPv4_enabled = "rpc.gui.acme.enabled",
   }
   content_helper.getExactContent(wan_services_data)
   wan_services_data["cert_IPv6_enabled"] = wan_services_data["cert_IPv4_enabled"]
 
-  local ddns4 = get_ddns_status(wan_services_data, "IPv4")
+  local ddns4 = get_ddns_status(wan_services_data,"IPv4")
   if ddns4 then
     html[#html+1] = ddns4
   end
-  local ddns6 = get_ddns_status(wan_services_data, "IPv6")
+  local ddns6 = get_ddns_status(wan_services_data,"IPv6")
   if ddns6 then
     html[#html+1] = ddns6
   end
   
   if wan_services_data["dmz_blocked"] == "1" then
-      html[#html+1] = ui_helper.createSimpleLight("2", T"DMZ blocked")
+      html[#html+1] = ui_helper.createSimpleLight("2",T"DMZ blocked")
   else
     if wan_services_data["dmz_enable"] == "1" then
-      html[#html+1] = ui_helper.createSimpleLight("1", T"DMZ enabled")
+      html[#html+1] = ui_helper.createSimpleLight("1",T"DMZ enabled")
     else
-      html[#html+1] = ui_helper.createSimpleLight("0", T"DMZ disabled")
+      html[#html+1] = ui_helper.createSimpleLight("0",T"DMZ disabled")
     end
   end
 
@@ -85,22 +91,22 @@ function M.getWANServicesCardHTML()
     wolenabled = proxy.get("uci.wol.config.enabled")
     if wolenabled then
       if wolenabled[1].value == "1" then
-        html[#html+1] = ui_helper.createSimpleLight("1", T"WoL over Internet enabled")
+        html[#html+1] = ui_helper.createSimpleLight("1",T"WoL over Internet enabled")
       else
-        html[#html+1] = ui_helper.createSimpleLight("0", T"WoL over Internet disabled")
+        html[#html+1] = ui_helper.createSimpleLight("0",T"WoL over Internet disabled")
       end
     end
   end
 
   local n_upnp_rules = tonumber(wan_services_data["upnp_rules"])
   if wan_services_data["upnp_status"] == "1" then
-    html[#html+1] = ui_helper.createSimpleLight("1", T"UPnP enabled")
+    html[#html+1] = ui_helper.createSimpleLight("1",T"UPnP enabled")
     html[#html+1] = '<p class="subinfos">'
-    html[#html+1] = format(N("<strong %s>%d UPnP rule</strong> is active", "<strong %s>%d UPnP rules</strong> are active", n_upnp_rules),
-              'class="modal-link" data-toggle="modal" data-remote="modals/wanservices-modal.lp" data-id="wanservices-modal"', n_upnp_rules)
+    html[#html+1] = format(N("<strong %s>%d UPnP rule</strong> is active","<strong %s>%d UPnP rules</strong> are active",n_upnp_rules),
+              'class="modal-link" data-toggle="modal" data-remote="modals/wanservices-modal.lp" data-id="wanservices-modal"',n_upnp_rules)
     html[#html+1] = '</p>'
   else
-    html[#html+1] = ui_helper.createSimpleLight("0", T"UPnP disabled")
+    html[#html+1] = ui_helper.createSimpleLight("0",T"UPnP disabled")
   end
 
   return html
