1a6bb903993a8209a8c578aba4b87b2109556140
seud0nym <seud0nym@yahoo.com.au>
2022-01-26 17:52:24 +1100

    Missed vendor extensions

--- utilities/restore-config/400-multiap.sh
+++ utilities/restore-config/400-multiap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -e /etc/config/multiap -a -e $BANK2/etc/config/multiap ]; then
-  log I "Restoring multiap configuration..."
+  log I "Restoring MultiAP configuration..."
   agent_mac="$(uci -q get multiap.agent.macaddress)"
   ctrlr_mac="$(uci -q get multiap.controller.macaddress)"
   uci -q revert multiap
-  restore_file /etc/config/multiap
+  restore_file /etc/config/multiap /etc/config/vendorextensions
   uci_set multiap.agent.macaddress="$agent_mac"
   uci_set multiap.controller.macaddress="$ctrlr_mac"
   uci -q commit multiap
