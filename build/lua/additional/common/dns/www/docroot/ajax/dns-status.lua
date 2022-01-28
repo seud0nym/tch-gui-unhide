be486f6ffe3788c88352f35e4d6b34c89a1e69a8
seud0nym <seud0nym@yahoo.com.au>
2022-01-29 10:41:44 +1100

    Moved DNS Hijacking to DNS card

--- build/lua/additional/common/dns/www/docroot/ajax/dns-status.lua
+++ build/lua/additional/common/dns/www/docroot/ajax/dns-status.lua
@@ -11,7 +11,6 @@ local split = require("split").split
 
 local dnsmasq_path = dns_helper.dnsmasq_path
 local dnsmasq_enabled = "0"
-local dnsmasq_status = "Custom DNS disabled"
 local dns_servers = {}
 local rewrites_count = 0
 
@@ -19,7 +18,6 @@ local noresolv = proxy.get(dnsmasq_path.."noresolv")[1].value
 local disabled = proxy.get(dnsmasq_path.."disabled")
 if not disabled or disabled[1].value == "0" then
   dnsmasq_enabled = "1"
-  dnsmasq_status = "Custom DNS enabled"
   local addresses = proxy.getPN(dnsmasq_path.."address.",true)
   rewrites_count = #addresses
   for _,dnsmsrvr in pairs(proxy.get(dnsmasq_path.."server.")) do
@@ -35,7 +33,6 @@ end
 if #dns_servers == 0 or noresolv ~= "1" then
   if dnsmasq_enabled == "1" and #dns_servers == 0 then
     dnsmasq_enabled = "0"
-    dnsmasq_status = "Custom DNS not configured"
   end
   local interface_pn = proxy.getPN("rpc.network.interface.",true)
   local dnsservers_paths = {}
@@ -62,11 +59,10 @@ local rewrites_modal_link = 'class="modal-link" data-toggle="modal" data-remote=
 
 local html = {}
 
-html[#html+1] = ui_helper.createSimpleLight(dnsmasq_enabled,dnsmasq_status)
 html[#html+1] = '<span class="simple-desc"><i>&nbsp</i>'
 html[#html+1] = format(N('<strong %s>%d</strong><span %s> DNS Server:</span>','<strong %s>%d</strong><span %s> DNS Servers:</span>',#dns_servers),dns_modal_link,#dns_servers,dns_modal_link)
 html[#html+1] = '</span><p class="subinfos" style="letter-spacing:-1px;font-size:12px;font-weight:bold;line-height:15px;">'
-local max_show = 3
+local max_show = 2
 for i,server in ipairs(dns_servers) do
   if i > 1 and i <= max_show then
     html[#html+1] = '<br>'
@@ -81,6 +77,55 @@ end
 html[#html+1] = '</p><span class="simple-desc"><i>&nbsp</i>'
 html[#html+1] = format(N('<strong%s >%d</strong><span %s> DNS Rewrite</span>','<strong %s>%d</strong><span %s> DNS Rewrites</span>',rewrites_count),rewrites_modal_link,rewrites_count,rewrites_modal_link)
 html[#html+1] = '</span>'
+
+local interceptd = proxy.get("uci.intercept.config.enabled","uci.intercept.dns.spoofip")
+local interceptd_enabled
+local interceptd_status
+if interceptd and interceptd[1].value == "0" then
+  interceptd_enabled = "0"
+  interceptd_status = "disabled"
+else
+  interceptd_enabled = "1"
+  interceptd_status = "enabled"
+end
+html[#html+1] = ui_helper.createSimpleLight(interceptd_enabled,T(format('<span class="modal-link" data-toggle="modal" data-remote="/modals/dns-interceptd-modal.lp" data-id="dns-interceptd-modal">Intercept Daemon %s</span>',interceptd_status)))
+
+local dns6_int_state = proxy.get("uci.tproxy.rule.@dnsv6.enabled")
+local dns4_int_state = "0"
+local dns_int_modal_link = 'class="modal-link" data-toggle="modal" data-remote="/modals/firewall-dns_int-modal.lp" data-id="firewall-dns_int-modal"'
+for _,v in ipairs(proxy.getPN("uci.firewall.redirect.",true)) do
+  local path = v.path
+  local values = proxy.get(path.."name",path.."enabled")
+  if values then
+    local name = values[1].value
+    if name == "Redirect-DNS" or name == "Intercept-DNS" then
+      if values[2] and values[2].value ~= "0" then
+        dns4_int_state = "1"
+      end
+      break
+    end
+  end
+end
+if dns6_int_state then
+  dns6_int_state = dns6_int_state[1].value
+end
+local dns_int_state, dns_int_text, dns_int_family
+if dns4_int_state == "1" or dns6_int_state == "1" then
+  dns_int_state = "1"
+  dns_int_text = "enabled"
+  if dns4_int_state == "1" and dns6_int_state == "1" then
+    dns_int_family = ""
+  elseif dns4_int_state == "1" then
+    dns_int_family = "IPv4"
+  else
+    dns_int_family = "IPv6"
+  end
+else
+  dns_int_state = "0"
+  dns_int_text = "disabled"
+  dns_int_family = ""
+end
+html[#html+1] = ui_helper.createSimpleLight(dns_int_state,T(format("<span %s>%s DNS Hijacking %s</span>",dns_int_modal_link,dns_int_family,dns_int_text)))
 
 local data = {
   html = table.concat(html,"\n"),
