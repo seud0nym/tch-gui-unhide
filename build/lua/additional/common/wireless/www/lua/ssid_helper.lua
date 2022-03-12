dc7bd0f18dc91b7e49d681779d339b480389f3fe
seud0nym <seud0nym@yahoo.com.au>
2022-03-13 09:49:12 +1100

    Show WiFi channels on card (#78)

--- build/lua/additional/common/wireless/www/lua/ssid_helper.lua
+++ build/lua/additional/common/wireless/www/lua/ssid_helper.lua
@@ -8,89 +8,84 @@ local untaint = string.untaint
 
 local M = {}
 
-function M.getSSIDList()
-  local ssid_list = {}
-  local fon_count = 0
-  local fon_state = 0
-
-  for _,v in ipairs(proxy.getPN("rpc.wireless.ssid.",true)) do
-    local path = v.path
-    local values = proxy.get(path.."radio",path.."ssid",path.."oper_state",path.."network")
-    if values then
-      local ssid = values[2].value
-      if ssid:sub(1,3) ~= "BH-" then
-        if ssid == "Fon WiFi" or ssid == "Fon WiFi-5G" or ssid == "Telstra Air" or ssid == "Telstra Air-5G" then
-          fon_count = fon_count + 1
-          if values[3].value == 1 and fon_state == 0 then
-            fon_state = 1
-          end
-        else
-          local ap_display_name = proxy.get(path.."ap_display_name")[1].value
-          local radio_name = values[1].value
-          local tx_power_adjust = proxy.get("rpc.wireless.radio.@"..radio_name..".tx_power_adjust")
-          local radio_suffix
-          local display_ssid
-          local isguest
-          local sortby
-          if radio_name == "radio_2G" then
-            radio_suffix = format(" %s","(2.4G)")
-          else
-            radio_suffix = format(" %s","(5G)")
-          end
-          if ap_display_name ~= "" then
-            display_ssid = ap_display_name..radio_suffix
-          else
-            local stb = proxy.get(path.."stb")
-            if stb and stb[1].value == "1" then
-              display_ssid = "IPTV"..radio_suffix
-            else
-              display_ssid = ssid..radio_suffix
-            end
-          end
-          if values[4].value:sub(1,5) == "Guest" then
-            sortby = "yyyyy"
-            isguest = "1"
-          else
-            sortby = display_ssid:lower()
-            isguest = "0"
-          end
-          ssid_list[#ssid_list+1] = {
-            id = format("SSID%s",#ssid_list),
-            ssid = display_ssid,
-            isguest = isguest,
-            state = untaint(values[3].value),
-            sort = sortby,
-            network = untaint(values[4].value),
-            tx_power_adjust = tx_power_adjust and tx_power_adjust[1].value or "0",
-            iface = match(path,".*@([^.]*)"),
-          }
-        end
-      end
-    end
-  end
-
-  if fon_count > 0 then
-    ssid_list[#ssid_list+1] = {
-      id = format("SSID%s",#ssid_list),
-      ssid = T"Telstra Air ("..format(N("%s SSID","%s SSIDs"),fon_count,fon_count)..")",
-      state = fon_state,
-      sort = "zzzzz"
-    }
-  end
-
-  table.sort(ssid_list,function(a,b) return a.sort < b.sort end)
-
-  return ssid_list
-end
-
 function M.getWiFiCardHTML()
-  local ssid_list = M.getSSIDList()
+  local radios = {}
   local bs_lan = "disabled"
   local html = {}
   local bs = {}
   local ap = {}
   local hidden = {}
   local wps = {}
+  local band_attr = {
+    span = {
+      class = "wifi-band",
+    },
+  }
+  local ssid_attr = {
+    span = {
+      class = "ssid-status",
+    },
+  }
+
+  for _,v in ipairs(proxy.getPN("rpc.wireless.radio.", true)) do
+    local radio = match(v.path, "rpc%.wireless%.radio%.@([^%.]+)%.")
+    if radio then
+      local values = proxy.get(v.path.."band",v.path.."channel_width",v.path.."channel",v.path.."tx_power_adjust",v.path.."admin_state")
+      if values then
+        local info = format("%s Channel %s <sup class='wifi-width'>%s</sup>",values[1].value,values[3].value,values[2].value)
+        if values[4] and values[4].value ~= "" and values[4].value ~= "0" then
+          info = format("%s <span class='wifi-dBm'>(%s dBm)</span>",info,values[4].value)
+        end
+        radios[radio] = {
+          admin_state = untaint(values[5].value),
+          info = info,
+          ssid = {},
+        }
+      end
+    end
+  end
+
+  for _,v in ipairs(proxy.getPN("rpc.wireless.ssid.",true)) do
+    local path = v.path
+    local values = proxy.get(path.."radio",path.."ssid",path.."oper_state",path.."network")
+    if values then
+      local ssid = untaint(values[2].value)
+      if not (ssid:sub(1,3) == "BH-" or ssid == "Fon WiFi" or ssid == "Fon WiFi-5G" or ssid == "Telstra Air" or ssid == "Telstra Air-5G") then
+        local ap_display_name = untaint(proxy.get(path.."ap_display_name")[1].value)
+        local radio = untaint(values[1].value)
+        local display_ssid
+        local isguest
+        local sortby
+        if ap_display_name ~= "" then
+          display_ssid = ap_display_name
+        else
+          local stb = proxy.get(path.."stb")
+          if stb and stb[1].value == "1" then
+            display_ssid = "IPTV"
+          else
+            display_ssid = ssid
+          end
+        end
+        if values[4].value:sub(1,5) == "Guest" then
+          sortby = "yyyyy"
+          isguest = "1"
+        else
+          sortby = display_ssid:lower()
+          isguest = "0"
+        end
+        radios[radio]["ssid"][#radios[radio]["ssid"]+1] = {
+          id = format("SSID%s",#radios[radio]["ssid"]),
+          ssid = display_ssid,
+          isguest = isguest,
+          state = untaint(values[3].value),
+          sort = sortby,
+          network = untaint(values[4].value),
+          tx_power_adjust = radios[radio]["tx_power_adjust"],
+          iface = match(path,".*@([^.]*)"),
+        }
+      end
+    end
+  end
 
   for _,v in ipairs(proxy.getPN("uci.wireless.wifi-ap.",true)) do
     local path = v.path
@@ -120,27 +115,24 @@ function M.getWiFiCardHTML()
     end
   end
 
-  local ssid_attr = {
-    span = {
-      style = "height:20px;overflow:hidden;",
-    },
-  }
+  for _,radio in pairs(radios) do
+    table.sort(radio.ssid,function(a,b) return a.sort < b.sort end)
 
-  for i,v in ipairs(ssid_list) do
-    if bs_lan == "disabled" and v.network == "lan" and bs[v.iface] == "1" then
-      bs_lan = "enabled"
-    end
-    if i <= 5 then
-      local state
-      if v.iface and ap[v.iface] then
-        state = format("<span class='modal-link' data-toggle='modal' data-remote='/modals/wireless-qrcode-modal.lp?iface=%s&ap=%s' data-id='wireless-qrcode-modal' title='Click to display QR Code'%s>%s</span>%s",v.iface,ap[v.iface],hidden[v.iface],v.ssid,wps[v.iface])
-      else
-        state = v.ssid
+    html[#html+1] = ui_helper.createSimpleLight(radio.admin_state,radio.info,band_attr)
+
+    for i,v in ipairs(radio["ssid"]) do
+      if bs_lan == "disabled" and v.network == "lan" and bs[v.iface] == "1" then
+        bs_lan = "enabled"
       end
-      if v.tx_power_adjust and v.tx_power_adjust ~= "" and v.tx_power_adjust ~= "0" then
-        state = format("%s&nbsp;<span style='color:gray;font-size:xx-small;'>%s dBm</span>",state,v.tx_power_adjust)
+      if i <= 2 then
+        local state
+        if v.iface and ap[v.iface] then
+          state = format("<span class='modal-link' data-toggle='modal' data-remote='/modals/wireless-qrcode-modal.lp?iface=%s&ap=%s' data-id='wireless-qrcode-modal' title='Click to display QR Code'%s>%s</span>%s",v.iface,ap[v.iface],hidden[v.iface],v.ssid,wps[v.iface])
+        else
+          state = v.ssid
+        end
+        html[#html+1] = ui_helper.createSimpleLight(v.state or "0",state,ssid_attr)
       end
-      html[#html+1] = ui_helper.createSimpleLight(v.state or "0",state,ssid_attr)
     end
   end
 
