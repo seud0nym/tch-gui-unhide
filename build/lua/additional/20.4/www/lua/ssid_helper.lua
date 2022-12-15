6aba0402a61b578a2f7b9789c6598ac33efe3938
seud0nym <seud0nym@yahoo.com.au>
2022-12-15 13:26:02 +1100

    Fixed clicking on SSID to show QR Code not working

--- build/lua/additional/20.4/www/lua/ssid_helper.lua
+++ build/lua/additional/20.4/www/lua/ssid_helper.lua
@@ -8,6 +8,62 @@ local untaint = string.untaint
 
 local M = {}
 
+local function fetch_by_cred(paramindex,radios)
+  local radio,ssid,ishidden,isguest,state,sortby,wps_state
+  ssid = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.ssid",paramindex))[1].value)
+  state = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.state",paramindex))[1].value)
+
+  local frequency = sub(untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.frequency_bands",paramindex))[1].value),1,1)
+  for r,v in pairs(radios) do
+    if v.frequency == frequency then
+      radio = r
+      break
+    end
+  end
+
+  local password = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.wpa_psk_key",paramindex))[1].value)
+  for _,profile in ipairs(proxy.getPN("rpc.X_AIRTIES_Obj.MultiAPController.SSIDProfile.",true)) do
+    local path = profile.path
+    local profile_ssid = proxy.get(path.."SSID")
+    if profile_ssid and profile_ssid[1].value == ssid and password == proxy.get(path.."KeyPassphrase")[1].value then
+      ishidden = proxy.get(path.."Hide")[1].value == "true" and "0" or "1"
+      wps_state = proxy.get(path.."WPS")[1].value == "true" and "1" or "0"
+      break
+    end
+  end
+
+  if proxy.get(format("uci.mesh_broker.controller_credentials.@%s.type",paramindex))[1].value == "guest" then
+    sortby = "zzzz"
+    isguest = "1"
+  else
+    sortby = ssid:lower()
+    isguest = "0"
+  end
+  return radio,ssid,ishidden,isguest,state,sortby,wps_state
+end
+
+local function fetch_by_intf(paramindex)
+  local radio,ssid,ishidden,isguest,state,sortby,wps_state
+  local ap_display_name = untaint(proxy.get(format("rpc.wireless.ssid.@%s.ap_display_name",paramindex))[1].value)
+  if ap_display_name ~= "" then
+    ssid = ap_display_name
+  else
+    ssid = untaint(proxy.get(format("uci.wireless.wifi-iface.@%s.ssid",paramindex))[1].value)
+  end
+  radio = untaint(proxy.get(format("rpc.wireless.ssid.@%s.radio",paramindex))[1].value)
+  state = untaint(proxy.get(format("rpc.wireless.ap.@%s.oper_state",paramindex))[1].value)
+  ishidden = untaint(proxy.get(format("uci.wireless.wifi-iface.@%s.public",paramindex))[1].value)
+  wps_state = untaint(proxy.get(format("rpc.wireless.ap.@%s.wps.admin_state",paramindex))[1].value)
+  if proxy.get(format("uci.wireless.wifi-iface.@%s.network",paramindex))[1].value == "lan" then
+    sortby = ssid:lower()
+    isguest = "0"
+  else
+    sortby = "zzzz"
+    isguest = "1"
+  end
+  return radio,ssid,ishidden,isguest,state,sortby,wps_state
+end
+
 function M.isMultiAPEnabled()
   local multiap_state = {
     agent = "uci.wireless.map-agent.@agent.state",
@@ -15,7 +71,7 @@ function M.isMultiAPEnabled()
     meshbrokerState = "uci.mesh_broker.global.@mesh_broker.enable"
   }
   content_helper.getExactContent(multiap_state)
-  return multiap_state.agent == "0" and multiap_state.controller == "1" and multiap_state.meshbrokerState == "1"
+  return multiap_state.controller == "1" and multiap_state.meshbrokerState == "1" and (multiap_state.agent == "1" or multiap_state.agent == "0")
 end
 
 function M.getWiFiCardHTML()
@@ -48,68 +104,12 @@ function M.getWiFiCardHTML()
     end
   end
 
-  local function fetch_by_cred(paramindex)
-    local radio,ssid,ishidden,isguest,state,sortby,wps_state
-    ssid = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.ssid",paramindex))[1].value)
-    state = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.state",paramindex))[1].value)
-
-    local frequency = sub(untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.frequency_bands",paramindex))[1].value),1,1)
-    for r,v in pairs(radios) do
-      if v.frequency == frequency then
-        radio = r
-        break
-      end
-    end
-
-    local password = untaint(proxy.get(format("uci.mesh_broker.controller_credentials.@%s.wpa_psk_key",paramindex))[1].value)
-    for _,profile in ipairs(proxy.getPN("rpc.X_AIRTIES_Obj.MultiAPController.SSIDProfile.",true)) do
-      local path = profile.path
-      local profile_ssid = proxy.get(path.."SSID")
-      if profile_ssid and profile_ssid[1].value == ssid and password == proxy.get(path.."KeyPassphrase")[1].value then
-        ishidden = proxy.get(path.."Hide")[1].value == "true" and "0" or "1"
-        wps_state = proxy.get(path.."WPS")[1].value == "true" and "1" or "0"
-        break
-      end
-    end
-
-    if proxy.get(format("uci.mesh_broker.controller_credentials.@%s.type",paramindex))[1].value == "guest" then
-      sortby = "zzzz"
-      isguest = "1"
-    else
-      sortby = ssid:lower()
-      isguest = "0"
-    end
-    return radio,ssid,ishidden,isguest,state,sortby,wps_state
-  end
-
-  local function fetch_by_intf(paramindex)
-    local radio,ssid,ishidden,isguest,state,sortby,wps_state
-    local ap_display_name = untaint(proxy.get(format("rpc.wireless.ssid.@%s.ap_display_name",paramindex))[1].value)
-    if ap_display_name ~= "" then
-      ssid = ap_display_name
-    else
-      ssid = untaint(proxy.get(format("uci.wireless.wifi-iface.@%s.ssid",paramindex))[1].value)
-    end
-    radio = untaint(proxy.get(format("rpc.wireless.ssid.@%s.radio",paramindex))[1].value)
-    state = untaint(proxy.get(format("rpc.wireless.ap.@%s.oper_state",paramindex))[1].value)
-    ishidden = untaint(proxy.get(format("uci.wireless.wifi-iface.@%s.public",paramindex))[1].value)
-    wps_state = untaint(proxy.get(format("rpc.wireless.ap.@%s.wps.admin_state",paramindex))[1].value)
-    if proxy.get(format("uci.wireless.wifi-iface.@%s.network",paramindex))[1].value == "lan" then
-      sortby = ssid:lower()
-      isguest = "0"
-    else
-      sortby = "zzzz"
-      isguest = "1"
-    end
-    return radio,ssid,ishidden,isguest,state,sortby,wps_state
-  end
-
   for _,network in ipairs(proxy.getPN("uci.web.network.",true)) do
     local path = multiap_enabled and network.path.."cred." or network.path.."intf."
     for _,paramindex in ipairs(proxy.get(path)) do
       local radio,ssid,ishidden,isguest,state,sortby,wps_state,iface
       if multiap_enabled then
-        radio,ssid,ishidden,isguest,state,sortby,wps_state = fetch_by_cred(paramindex.value)
+        radio,ssid,ishidden,isguest,state,sortby,wps_state = fetch_by_cred(paramindex.value,radios)
         iface = proxy.get(paramindex.path:gsub("%.cred%.",".intf.").."value")[1].value
       else
         radio,ssid,ishidden,isguest,state,sortby,wps_state,iface = fetch_by_intf(paramindex.value)
