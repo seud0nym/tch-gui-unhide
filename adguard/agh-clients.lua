fcb353f71fadd15ff42b1be4af10e320604c5302
seud0nym <seud0nym@yahoo.com.au>
2023-01-08 16:26:18 +1100

    Keep client config (#124) + only update if changed

--- adguard/agh-clients.lua
+++ adguard/agh-clients.lua
@@ -1,4 +1,6 @@
 ---@diagnostic disable: lowercase-global
+json=require('dkjson')
+
 if (#arg % 2 ~= 0) then
   print("Required arguments: hostname:port username:password")
   print("                    Repeat hostname:port username:password for multiple servers")
@@ -13,21 +15,6 @@ for i = 1,#arg,2 do
   }
 end
 
-json=require('dkjson')
-
-cmd = io.popen('ubus call hostmanager.device get')
-raw = cmd:read('*a')
-cmd:close()
-hosts = json.decode(raw)
-
-device_ips = {}
-cmd = io.popen('echo -n $HOSTNAME')
-device_hostname = cmd:read('*a')
-cmd:close()
-cmd = io.popen('ip -o -br a | grep br-lan | tr -s " " | cut -d" " -f3-')
-device_ips = cmd:read('*a')
-cmd:close()
-
 function execute(cmd)
   local popen = io.popen(cmd)
   if popen then
@@ -36,6 +23,91 @@ function execute(cmd)
     return response
   end
   return nil
+end
+
+function getKnownDevices()
+  local devIPs = {}
+  local allIPs = {}
+
+  local devices = json.decode(execute('ubus call hostmanager.device get'))
+  table.sort(devices,function(a,b)
+    return string.lower(a.hostname) < string.lower(b.hostname)
+  end)
+
+  for _,host in pairs(devices) do
+    if host.interface == "lan" then
+      local hostname = host.hostname
+      if not hostname or hostname == "" then
+        hostname = host['user-friendly-name']
+        if not hostname or hostname == "" then
+          hostname = string.format("Unknown-%s",host['mac-address'])
+        end
+      end
+      local last2char = string.sub(hostname,-2)
+      if last2char == '-2' or last2char == '-3' or last2char == '-4' or last2char == '-5' then
+        hostname = string.sub(hostname,1,-3)
+      end
+      devIPs[hostname] = {}
+      for _,v4 in pairs(host.ipv4) do
+        table.insert(devIPs[hostname],v4.address)
+        allIPs[v4.address] = hostname
+      end
+      for _,v6 in pairs(host.ipv6) do
+        table.insert(devIPs[hostname],v6.address)
+        allIPs[v6.address] = hostname
+      end
+    end
+  end
+
+  local localhost = execute('echo -n $HOSTNAME') or "mymodem"
+  devIPs[localhost] = {}
+  for cidr in string.gmatch(execute('ip -o -br address show br-lan | tr -s " " | cut -d" " -f3-'),"([^%s]+)") do
+    local address = string.match(cidr,"([^/]+)/%d+")
+    table.insert(devIPs[localhost],address)
+    allIPs[address] = localhost
+  end
+
+  return devIPs,allIPs
+end
+
+devIPs,allIPs = getKnownDevices()
+
+function deduplicate(t)
+  local duplicate = {}
+  local result = {}
+
+  for _,v in pairs(t) do
+    if not duplicate[v] then
+      result[#result+1] = v
+      duplicate[v] = true
+    end
+  end
+
+  table.sort(result, function (a,b)
+    return string.lower(a) < string.lower(b)
+  end)
+
+  return result
+end
+
+function post(address,credentials,action,data)
+  local json = json.encode(data)
+  local result = execute(string.format('curl -su "%s" -X POST -H "Content-Type: application/json" --data \'%s\' http://%s/control/clients/%s',credentials,json,address,action))
+  if result and #result > 0 then
+    print("ERROR:",result)
+  end
+end
+
+function the_same(original,updated)
+  if original.count ~= #updated then
+    return false
+  end
+  for _,v in pairs(updated) do
+    if not original.hash[v] then
+      return false
+    end
+  end
+  return true
 end
 
 function process(target)
@@ -48,79 +120,57 @@ function process(target)
 
   local clients = {}
   local ids = {}
-  local index = {}
+  local names = {}
 
-  local raw = execute(string.format('curl -su "%s" http://%s/control/clients', credentials, address))
-  local current = json.decode(raw or {})
+  local clientids = {}
+
+  local current = json.decode(execute(string.format('curl -su "%s" http://%s/control/clients',credentials,address)) or {})
 
   if current.clients then
-    for i,client in pairs(current.clients) do
+    table.sort(current.clients,function(a,b)
+      return string.lower(a.name) < string.lower(b.name)
+    end)
+    for _,client in pairs(current.clients) do
       if client.ids then
-        delete[client.name] = true
-        local hostname
-        if string.sub(client.name,-2) == '-2' then
-          hostname = string.sub(client.name,1,-3)
-        else
-          hostname = client.name
-        end
-        index[hostname] = {
-          source = i,
+        local hostname = client.name
+        clientids[hostname] = {
+          count = #client.ids,
+          hash = {},
         }
-        if not clients[hostname] then
-          clients[hostname] = {
-            ids = {}
-          }
-        end
-        for _,id in pairs(client.ids) do
-          raw = execute(string.format('curl -su "%s" "http://%s/control/querylog?limit=1&search=%s"', credentials, address, id))
-          local querylog = json.decode(raw or {})
-          if #querylog.data > 0 then
-            clients[hostname].ids[#clients[hostname].ids+1] = id
-            ids[id] = hostname
+        for _,id in ipairs(client.ids)  do
+          clientids[hostname]["hash"][id] = true
+          if allIPs[id] then -- IP is associated with a current device
+            ids[id] = allIPs[id]
+          else -- See if the IP is in the AdGuard Home query log
+            local querylog = json.decode(execute(string.format('curl -su "%s" "http://%s/control/querylog?limit=1&search=%s"',credentials,address,id)) or {})
+            if #querylog.data > 0 then -- IP is in the query log
+              ids[id] = hostname
+            end
           end
         end
+        client.ids = {} -- Empty the list of client IDs
+        clients[hostname] = client -- Preserve all other settings
+        delete[hostname] = true -- Mark it as to be deleted, until we know otherwise
+        table.insert(names,hostname)
       end
     end
   end
 
-  delete[device_hostname] = false
-  if not clients[device_hostname] then
-    clients[device_hostname] = {
-      ids = {}
-    }
-  end
-  for ip in string.gmatch(device_ips, "(%S+)/%d%d") do
-    clients[device_hostname].ids[#clients[device_hostname].ids+1] = ip
-    ids[ip] = device_hostname
-  end
-
-  for _,host in pairs(hosts) do
-    if host.interface == "lan" then
-      local mac = host['mac-address']
-      local hostname = host.hostname
-      if not hostname or hostname == "" then
-        hostname = host['user-friendly-name']
-        if not hostname or hostname == "" then
-          hostname = string.format("Unknown-%s", mac)
-        end
-      end
-      if string.sub(hostname,-2) == '-2' then
-        hostname = string.sub(hostname,1,-3)
-      end
-      delete[hostname] = false
-      for _,ipv4 in pairs(host.ipv4) do
-        ids[ipv4.address] = hostname
-      end
-      for _,ipv6 in pairs(host.ipv6) do
-        ids[ipv6.address] = hostname
-      end
+  for devicename,ips in pairs(devIPs) do
+    delete[devicename] = nil -- Mark it as NOT to be deleted
+    table.insert(names,devicename)
+    for _,id in pairs(ips) do
+      ids[id] = devicename
     end
   end
 
-  for id, hostname in pairs(ids) do
-    local i = index[hostname]
-    if not i then
-      append[#append+1] = {
+  for id,hostname in pairs(ids) do
+    local client = clients[hostname]
+    if client then
+      update[hostname] = not append[hostname]
+    else
+      append[hostname] = true
+      client = {
         name = hostname,
         use_global_settings = true,
         filtering_enabled = true,
@@ -130,52 +180,37 @@ function process(target)
         use_global_blocked_services = true,
         ids = {}
       }
-      index[hostname] = {
-        target = "A",
-        index = #append
-      }
-    elseif not i.target then
-      update[#update+1] = {
-        name = hostname,
-        data = {
-          use_global_settings = true,
-          filtering_enabled = true,
-          parental_enabled = true,
-          safebrowsing_enabled = true,
-          safesearch_enabled = true,
-          use_global_blocked_services = true,
-          name = hostname,
-          ids = {}
-        }
-      }
-      index[hostname] = {
-        target = "U",
-        index = #update
-      }
+      clients[hostname] = client
     end
-    if index[hostname].target == "A" then
-      append[index[hostname].index].ids[#append[index[hostname].index].ids+1] = id
-    else
-      update[index[hostname].index].data.ids[#update[index[hostname].index].data.ids+1] = id
-    end
+    table.insert(client.ids,id)
   end
 
-  for host,v in pairs(delete) do
-    if v then
-      print("Removing client "..host.." from "..address)
-      data = json.encode({ name = host})
-      print(execute(string.format('curl -su "%s" -X POST -H "Content-Type: application/json" --data \'%s\' http://%s/control/clients/delete', credentials, data, address)))
+  names = deduplicate(names)
+
+  for _,hostname in ipairs(names) do
+    if delete[hostname] then
+      print("Deleting AdGuard Client "..hostname.." from "..address)
+      post(address,credentials,"delete",{
+        name = hostname
+      })
     end
   end
-  for _,host in pairs(append) do
-    print("Adding client "..host.name.." to "..address)
-    data = json.encode(host)
-    print(execute(string.format('curl -su "%s" -X POST -H "Content-Type: application/json" --data \'%s\' http://%s/control/clients/add', credentials, data, address)))
+  for _,hostname in ipairs(names) do
+    if append[hostname] then
+      print("Inserting AdGuard Client "..hostname.." to "..address)
+      table.sort(clients[hostname].ids)
+      post(address,credentials,"add",clients[hostname])
+    end
   end
-  for _,host in pairs(update) do
-    print("Updating client "..host.name.." on "..address)
-    data = json.encode(host)
-    print(execute(string.format('curl -su "%s" -X POST -H "Content-Type: application/json" --data \'%s\' http://%s/control/clients/update', credentials, data, address)))
+  for _,hostname in ipairs(names) do
+    if update[hostname] and not the_same(clientids[hostname],clients[hostname].ids) then
+      print("Updating AdGuard Client "..hostname.." on "..address)
+      table.sort(clients[hostname].ids)
+      post(address,credentials,"update",{
+        name = hostname,
+        data = clients[hostname]
+      })
+    end
   end
 end
 
