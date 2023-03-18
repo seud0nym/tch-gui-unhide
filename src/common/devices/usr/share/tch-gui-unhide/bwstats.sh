1dbb1bf85cb5ac681d3dc4179aa3000dcbf5c20c
seud0nym <seud0nym@yahoo.com.au>
2023-03-18 17:40:10 +1100

    Fixed various bugs

--- src/common/devices/usr/share/tch-gui-unhide/bwstats.sh
+++ src/common/devices/usr/share/tch-gui-unhide/bwstats.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+TABLE=mangle
+PARENT_CHAIN=FORWARD
+
 if [ "/$1/" = "/show/" ]; then
   for CHAIN in BWSTATSRX BWSTATSTX; do
     for CMD in iptables ip6tables; do
-      $CMD -t mangle -nvL $CHAIN
-      $CMD -t mangle -nvL FORWARD | grep $CHAIN
+      $CMD -t $TABLE -nvL $CHAIN
+      $CMD -t $TABLE -nvL $PARENT_CHAIN | grep $CHAIN
       echo
     done
   done
@@ -15,16 +18,18 @@ else
   if [ "/$1/" = "/disable/" -o "/$(uci -q get bwstats.config.enabled)/" != "/1/" ]; then
     PARAMETER_COUNT="$(iptables -t $TABLE -S $PARENT_CHAIN | grep -m 1 BWSTATS | wc -w)"
     for CMD in iptables ip6tables; do
-      $CMD -t mangle -S FORWARD | sed -e '/BWSTATS/!d' -e 's/-A/-D/' | xargs -rn $PARAMETER_COUNT $CMD -t mangle
-      $CMD -t mangle -nL BWSTATSRX >/dev/null 2>&1 && { $CMD -t mangle -F BWSTATSRX; $CMD -t mangle -X BWSTATSRX; }
-      $CMD -t mangle -nL BWSTATSTX >/dev/null 2>&1 && { $CMD -t mangle -F BWSTATSTX; $CMD -t mangle -X BWSTATSTX; }
+      [ $PARAMETER_COUNT -gt 0 ] && $CMD -t $TABLE -S $PARENT_CHAIN | sed -e '/BWSTATS/!d' -e 's/-A/-D/' | xargs -rn $PARAMETER_COUNT $CMD -t $TABLE
+      $CMD -t $TABLE -nL BWSTATSRX >/dev/null 2>&1 && { $CMD -t $TABLE -F BWSTATSRX; $CMD -t $TABLE -X BWSTATSRX; }
+      $CMD -t $TABLE -nL BWSTATSTX >/dev/null 2>&1 && { $CMD -t $TABLE -F BWSTATSTX; $CMD -t $TABLE -X BWSTATSTX; }
     done
   else
-    for CMD in iptables ip6tables; do
-      $CMD -t mangle -nL BWSTATSRX >/dev/null 2>&1 || $CMD -t mangle -N BWSTATSRX
-      $CMD -t mangle -C FORWARD -i br-lan -j BWSTATSRX 2>/dev/null || $CMD -t mangle -I FORWARD 1 -i br-lan -j BWSTATSRX
-      $CMD -t mangle -nL BWSTATSTX >/dev/null 2>&1 || $CMD -t mangle -N BWSTATSTX
-      $CMD -t mangle -C FORWARD -o br-lan -j BWSTATSTX 2>/dev/null || $CMD -t mangle -I FORWARD 1 -o br-lan -j BWSTATSTX
+    for CHAIN in BWSTATSRX BWSTATSTX; do
+      [ $CHAIN = BWSTATSRX ] && DIRECTION=i || DIRECTION=o
+      for CMD in iptables ip6tables; do
+        $CMD -t $TABLE -nL $CHAIN >/dev/null 2>&1 || $CMD -t $TABLE -N $CHAIN
+        $CMD -t $TABLE -C $CHAIN -j LOG --log-prefix "$CHAIN IGNORED! " 2>/dev/null || $CMD -t $TABLE -A $CHAIN -j LOG --log-prefix "$CHAIN IGNORED! "
+        $CMD -t $TABLE -C $PARENT_CHAIN -$DIRECTION br-lan -j $CHAIN 2>/dev/null || $CMD -t $TABLE -I $PARENT_CHAIN 1 -$DIRECTION br-lan -j $CHAIN
+      done
     done
     /usr/share/tch-gui-unhide/bwstats.lua $(uci -q get bwstats.config.log_level) >/dev/null 2>&1 &
   fi
