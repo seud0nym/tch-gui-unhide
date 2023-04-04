b7f58cacf45ed653be71d2e4fa469da200d47fdb
seud0nym <seud0nym@yahoo.com.au>
2023-04-04 18:00:22 +1000

    Show wait spinner on tab change on slow screens

--- src/common/helpers/www/docroot/js/modal-busy.js
+++ src/common/helpers/www/docroot/js/modal-busy.js
@@ -1,5 +1,5 @@
 $('div.modal-body').after('<div class="loading-wrapper hide"><img src="/img/spinner.gif" /></div>');
-$("#save-config,.btn-table-edit,.btn-table-delete,.btn-table-modify,.btn-table-cancel,.btn-table-new").on("click",function(){
+$("#save-config,.btn-table-edit,.btn-table-delete,.btn-table-modify,.btn-table-cancel,.btn-table-new,ul.nav li a").on("click",function(){
   let busy_msg = $(".loading-wrapper");
   busy_msg.removeClass("hide");
   busy_msg[0].scrollIntoView();
