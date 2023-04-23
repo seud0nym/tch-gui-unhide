4bf6e55fe7bdab310f52ad9a6d0e1d58f80f440b
seud0nym <seud0nym@yahoo.com.au>
2023-04-23 15:49:05 +1000

    Converted showLoadingWrapper to callable function

--- src/common/helpers/www/docroot/js/modal-busy.js
+++ src/common/helpers/www/docroot/js/modal-busy.js
@@ -1,8 +1,9 @@
-$('div.modal-body').after('<div class="loading-wrapper hide"><img src="/img/spinner.gif" /></div>');
-$("#save-config,.btn-table-edit,.btn-table-delete,.btn-table-modify,.btn-table-cancel,.btn-table-new,ul.nav li a").on("click",function(){
+function showLoadingWrapper(){
   let busy_msg = $(".loading-wrapper");
   busy_msg.removeClass("hide");
   busy_msg[0].scrollIntoView();
   $(".modal-body").scrollLeft(0);
   return true;
-});
+}
+$('div.modal-body').after('<div class="loading-wrapper hide"><img src="/img/spinner.gif" /></div>');
+$("#save-config,.btn-table-edit,.btn-table-delete,.btn-table-modify,.btn-table-cancel,.btn-table-new,ul.nav li a").on("click",showLoadingWrapper);
