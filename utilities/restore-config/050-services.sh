1909ebb9b2b78a7f28f6854df1e0cb7fc9934083
seud0nym <seud0nym@yahoo.com.au>
2023-06-13 10:07:18 +1000

    Updated list of services to stop during restore

--- utilities/restore-config/050-services.sh
+++ utilities/restore-config/050-services.sh
@@ -2,20 +2,21 @@
 
 log I "Stopping services..."
 services="watchdog-tch \
-          autoreset \
-          bulkdata \
+          adblock autoreset \
+          bqos bulkdata \
           cron cupsd cwmpd cwmpdboot \
           ddns dlnad dosprotect dumaos dumaos_qos_tweaks \
           hotspotd \
-          intercept iperf ipset \
-          led ledfw lotagent lte-doctor-logger \
-          miniupnpd-tch mmpbxbrcmdect mmpbxd mmpbxd_lite mmpbxfwctl mosquitto mqttjson-services mud multiap_agent multiap_controller multiap_vendorextensions mwan \
+          intercept iperf ipset iqos \
+          led ledfw log lotagent lsyslogd lte-doctor-logger \
+          mesh-broker minidlna miniupnpd-tch mmpbxbrcmdect mmpbxd mmpbxd_lite mmpbxfwctl mosquitto mqttjson-services mud multiap_agent multiap_controller multiap_vendorextensions mwan \
           ndhttpd ndproxy nfcd nginx nqe \
           pinholehelper \
-          qos \
-          ra \
-          samba samba-nmbd socat supervision sysntpd \
-          telemetry-daemon tod trafficmon transformer \
+          qos qos_tch \
+          ra redirecthelper rsyncd \
+          samba samba-nmbd socat supervision sync-adguard-home-data sysntpd \
+          telemetry-daemon tls-thor tod trafficmon transformer tproxy \
+          uhttpd \
           wansensing wfa-testsuite-daemon wifi-doctor-agent wol \
           xl2tpd"
 
