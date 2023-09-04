70a499e616ea7d99b8c74c49667f12c028d3b518
seud0nym <seud0nym@yahoo.com.au>
2023-09-04 10:30:49 +1000

    Fix Adblock package restore

--- utilities/restore-config/700-packages.sh
+++ utilities/restore-config/700-packages.sh
@@ -11,19 +11,18 @@ find $BANK2/usr/lib/opkg/info/ -type c -name '*.control' -exec basename {} .cont
 
 if [ -s $BANK2/etc/opkg/customfeeds.conf -a $(grep -v '^#' $BANK2/etc/opkg/customfeeds.conf 2>/dev/null | wc -l) -gt 0 ]; then
   log I "Installing any additional packages"
-  if [ "$DEVICE_VERSION" = "17.2" -a -f $BANK2/usr/lib/opkg/info/adblock.control ]; then
-    download https://repository.macoers.com/homeware/18/brcm63xx-tch/VANTW/packages/adblock_3.5.5-4_all.ipk /tmp
-    opkg install /tmp/adblock_3.5.5-4_all.ipk
+  if [ -f $BANK2/usr/lib/opkg/info/adblock.control ]; then
+    download https://raw.githubusercontent.com/seud0nym/tch-gui-unhide/master/extras/tch-gui-unhide-xtra.adblock $SCRIPT_DIR
+    run_script $SCRIPT_DIR/tch-gui-unhide-xtra.adblock setup
   fi
+
+  exclude="! -name adblock.control"
   if [ "$DEVICE_VERSION" != "17.2" -a "$DEVICE_VERSION" != "18.1.c" ]; then
-    find $BANK2/usr/lib/opkg/info/ -type f -name '*.control' ! -name "samba36-server.control" -exec basename {} .control \; | xargs -r opkg --force-overwrite install
-  else
-    find $BANK2/usr/lib/opkg/info/ -type f -name '*.control' -exec basename {} .control \; | xargs -r opkg --force-overwrite install
+    exclude="$exclude ! -name samba36-server.control"
   fi
+  find $BANK2/usr/lib/opkg/info/ -type f -name '*.control' $exclude -exec basename {} .control \; | xargs -r opkg --force-overwrite install
+  unset exclude
+
   log I "Restoring any additional packages configuration"
   restore_file /etc/config/adblock /etc/config/minidlna /etc/rsyncd.conf
-  if [ "$DEVICE_VERSION" = "17.2" -a -f $BANK2/usr/lib/opkg/info/adblock.control ]; then
-    uci set adblock.global.adb_fetchutil='curl'
-    uci -q commit adblock
-  fi
 fi
