4558c8be359bc19048dcd3010edb69ee59ddb950
Stephen Stevens <seud0nym@yahoo.com.au>
2024-01-24 14:35:05 +1100

    Added opkg update & only restore if necessary

--- utilities/restore-config/700-packages.sh
+++ utilities/restore-config/700-packages.sh
@@ -10,21 +10,26 @@ log I "Removing any deleted packages"
 find $BANK2/usr/lib/opkg/info/ -type c -name '*.control' -exec basename {} .control \; | xargs -rt opkg --force-depends remove
 
 if [ -s $BANK2/etc/opkg/customfeeds.conf -a $(grep -v '^#' $BANK2/etc/opkg/customfeeds.conf 2>/dev/null | wc -l) -gt 0 ]; then
-  log I "Installing any additional packages"
+  log I "Checking for additional packages"
   if [ -f $BANK2/usr/lib/opkg/info/adblock.control ]; then
+    log I "Installing adblock"
     download https://raw.githubusercontent.com/seud0nym/tch-gui-unhide/master/extras/tch-gui-unhide-xtra.adblock $SCRIPT_DIR
     run_script $SCRIPT_DIR/tch-gui-unhide-xtra.adblock setup
   fi
 
-  exclude="! -name adblock.control"
-  if [ "$DEVICE_VERSION" != "17.2" -a "$DEVICE_VERSION" != "18.1.c" ]; then
-    exclude="$exclude ! -name samba36-server.control"
+  exclude="! -name adblock.control ! -name ca-certificates.control ! -name ca-bundle.control"
+  [ "$DEVICE_VERSION" != "17.2" -a "$DEVICE_VERSION" != "18.1.c" ] && exclude="$exclude ! -name samba36-server.control"
+  include=$(find $BANK2/usr/lib/opkg/info/ -type f -name '*.control' $exclude -exec basename {} .control \; | xargs)
+  if [ -n "$include" ]; then
+    log I "Installing additional packages"
+    opkg update
+    opkg --force-overwrite install $include
+    log I "Restoring additional packages configuration"
+    restore_file /etc/config/adblock /etc/config/minidlna /etc/rsyncd.conf
+  else
+    log I "No additional packages found to install"
   fi
-  find $BANK2/usr/lib/opkg/info/ -type f -name '*.control' $exclude -exec basename {} .control \; | xargs -r opkg --force-overwrite install
-  unset exclude
-
-  log I "Restoring any additional packages configuration"
-  restore_file /etc/config/adblock /etc/config/minidlna /etc/rsyncd.conf
+  unset exclude include
 fi
 
 if [ -e $BANK2/etc/nginx/OpenSpeedTest-Server.conf ]; then
