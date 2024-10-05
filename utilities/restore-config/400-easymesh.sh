eaa28cbf26be255f50b34ebe0c500cac7380d3aa
Stephen Stevens <seud0nym@yahoo.com.au>
2024-10-05 12:18:30 +1000

    Do full restore of mesh_broker

--- utilities/restore-config/400-easymesh.sh
+++ utilities/restore-config/400-easymesh.sh
@@ -14,21 +14,5 @@ fi
 
 if [ -e /etc/config/mesh_broker -a -e $BANK2/etc/config/mesh_broker ]; then
   log I "Restoring EasyMesh configuration..."
-  uci_set mesh_broker.mesh_broker.enable
-  uci_set mesh_broker.mesh_common.agent_enabled
-  uci_set mesh_broker.mesh_common.controller_enabled
-  source_path=$($UCI show mesh_broker | grep "backhaul='1'" | cut -d. -f1-2)
-  log D " >> Backhaul source path: $source_path"
-  target_path=$(uci show mesh_broker | grep "backhaul='1'" | cut -d. -f1-2)
-  log D " >> Backhaul target path: $target_path"
-  if [ -n "$source_path" -a -n "$target_path" ]; then
-    $UCI show $source_path | while read -r cfg; do
-      value=$(echo "$cfg" | cut -d= -f2-)
-      c=$(echo "$cfg" | cut -d= -f1 | cut -d. -f3)
-      uci_set "${target_path}.$c=$value"
-    done
-    unset cfg value c
-  fi
-  uci -q commit mesh_broker
-  unset source_path target_path
+  restore_file /etc/config/mesh_broker
 fi
