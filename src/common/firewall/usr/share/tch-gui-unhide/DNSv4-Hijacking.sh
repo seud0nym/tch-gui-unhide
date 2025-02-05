6c373bbcd85c0a38d0ec7c1816643ae11fb550f1
Stephen Stevens <seud0nym@yahoo.com.au>
2025-02-05 14:32:13 +1100

    Renamed DNS Hijacking firewall scripts

--- src/common/firewall/usr/share/tch-gui-unhide/DNSv4-Hijacking.sh
+++ src/common/firewall/usr/share/tch-gui-unhide/DNSv4-Hijacking.sh
@@ -2,8 +2,9 @@
 
 . $IPKG_INSTROOT/lib/functions.sh
 
-_DNS4=""
-
+_DNS4="$(ip -4 -o a show dev br-lan scope global | awk '{split($4,a,"/");print a[1]}' | xargs)"
+_ZONES="lan"
+  
 find_dhcp_option() {
   local value="$1"
   local ip="${value#6,}"
@@ -17,8 +18,20 @@ find_dns_servers() {
   config_list_foreach "$dhcp" "dhcp_option" find_dhcp_option
 }
 
+find_guest_zones() {
+  local config="$1"
+  local zone enabled wan
+  config_get zone "$config" "name"
+  [ "$zone" = "loopback" -o "$zone" = "lan" ] && return
+  config_get enabled "$config" "enabled" "1"
+  [ "$enabled" != "0" ] || return
+  config_get wan "$config" "wan" "0"
+  [ "$wan" = "1" ] && return
+  _ZONES="$_ZONES $zone"
+}
+
 # Delete any existing rules
-iptables -t nat -S zone_lan_prerouting | grep "No-Hijack-DHCP-DNSv4" | while read action params; do
+iptables -t nat -S | grep "DNSv4-Hijacking" | while read action params; do
   iptables -t nat -D $params
 done
 
@@ -28,21 +41,24 @@ config_get _HIJACK4 "dns_int" "enabled" "0"
   
 [ "$_HIJACK4" = "1" ] || exit
 
-# Load the device IPs if no proxy specified
-[ -z "$_DNS4" ] && _DNS4="$(ip -4 -o a show dev br-lan scope global | awk '{split($4,a,"/");print a[1]}' | xargs)"
+# Find Guest zones
+config_foreach find_guest_zones "zone"
+
 # Load the IP addresses of DNS servers configured via DHCP
 config_load dhcp
 config_foreach find_dns_servers "dhcp"
 
 # First insert the logging rules
 iptables -t nat -S | grep -E '(Redirect|Intercept)-DNS' | sed -e 's/-m comment.*$//' | while read action params; do 
-  iptables -t nat -I $params -m comment --comment "No-Hijack-DHCP-DNSv4" -j LOG --log-prefix '** HIJACKED ** ' 
+  iptables -t nat -I $params -m comment --comment "DNSv4-Hijacking" -j LOG --log-prefix '** HIJACKED ** ' 
 done
 
 # Create the rules to always accept these DNS servers
 for dns in $_DNS4; do
   for proto in udp tcp; do
-    iptables -t nat -I zone_lan_prerouting -d $dns/32 -p $proto -m $proto --dport 53 -m comment --comment "No-Hijack-DHCP-DNSv4" -j ACCEPT 2>/dev/null
+    for zone in $_ZONES; do
+      iptables -t nat -I zone_${zone}_prerouting -d $dns/32 -p $proto -m $proto --dport 53 -m comment --comment "DNSv4-Hijacking" -j ACCEPT 2>/dev/null
+    done
   done
 done
 
--- src/common/firewall/usr/share/tch-gui-unhide/DNSv6-Hijacking.sh
+++ src/common/firewall/usr/share/tch-gui-unhide/DNSv6-Hijacking.sh
@@ -15,7 +15,7 @@ find_dns_servers() {
 }
 
 # Delete any existing rules
-ip6tables -t mangle -S PREROUTING | grep "No-Hijack-DHCP-DNSv6" | while read action params; do
+ip6tables -t mangle -S PREROUTING | grep "DNSv6-Hijacking" | while read action params; do
   ip6tables -t mangle -D $params
 done
 
@@ -34,13 +34,13 @@ config_foreach find_dns_servers "dhcp"
 
 # First insert the logging rules
 ip6tables -t mangle -S PREROUTING | grep '!tproxy-go/dnsv6@.*-j TPROXY' | sed -e 's/-m comment.*$//' | while read action params; do 
-  ip6tables -t mangle -I $params  -m comment --comment "No-Hijack-DHCP-DNSv6" -j LOG --log-prefix '** HIJACKED ** ' 
+  ip6tables -t mangle -I $params  -m comment --comment "DNSv6-Hijacking" -j LOG --log-prefix '** HIJACKED ** ' 
 done
 
 # Create the rules to always accept these DNS servers
 for dns in $_DNS6; do
   for proto in udp tcp; do
-    ip6tables -t mangle -I PREROUTING -d $dns/128 -p $proto -m $proto --dport 53 -m comment --comment "No-Hijack-DHCP-DNSv6" -j ACCEPT 2>/dev/null
+    ip6tables -t mangle -I PREROUTING -d $dns/128 -p $proto -m $proto --dport 53 -m comment --comment "DNSv6-Hijacking" -j ACCEPT 2>/dev/null
   done
 done
 
